22e004b2a06f78f152a1454fe7cbe31b281a0220
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2024-11-19 19:46:12 +0100

    change app title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">My Website</a>
+                <a className="navbar-brand" href="#">React commerce</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
